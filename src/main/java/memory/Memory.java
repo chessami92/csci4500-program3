ad8e0e7f4ad1ec23504748156eb24ad5743d2c31
Josh DeWitt <chessami92@gmail.com>
2013-06-12 23:19:25 -0500

    Memory class now internally uses log2.

--- src/main/java/memory/Memory.java
+++ src/main/java/memory/Memory.java
@@ -2,13 +2,16 @@ package memory;
 
 public class Memory {
     public static int minBlockSize;
-    private int address = 0;
-    private int size;
+    /* Which request ID is holding this memory block. */
     protected int allocatedBy;
+    /* Address of this memory block. */
+    private int address = 0;
+    /* Size, stored as log2 of the actual size. */
+    private int size;
 
     /* Create the initial, unallocated memory block. */
     public Memory(int size) {
-        this.size = size;
+        this.size = convertToPowerOfTwo(size);
         allocatedBy = 0;
     }
 
@@ -18,32 +21,41 @@ public class Memory {
         this.address = address;
     }
 
+    /* Take in an integer value, and quickly convert it to a power of two. */
+    /* Will not work on integers that are not exact powers of two.         */
+    public int convertToPowerOfTwo(int num) {
+        int power = 0;
+
+        while (num >> 1 != 0) {
+            power++;
+            num = num >> 1;
+        }
+
+        return power;
+    }
+
     /* Creates and returns a new memory region split from this one. */
     /* Returns null if the memory cannot be split because it is     */
     /* allocated or is already at the minimum size.The size of this */
     /* memory and the new memory are half the original size of the  */
     /* current memory block.                                        */
     public Memory split() {
-        if (allocatedBy != 0 || size == minBlockSize) {
+        if (allocatedBy != 0 || getSize() == minBlockSize) {
             return null;
         }
 
-        size = size / 2;
-        return new Memory(address + size, size);
+        /* Size is stored as power of two, decrement to halve size. */
+        size--;
+        return new Memory(address + getSize(), getSize());
     }
 
     /* Takes a memory block and attempts to merge them. */
     /* Returns null if they cannot be merged, returns   */
     /* this resized merged memory block if successful.  */
     public Memory merge(Memory memory) {
-        /* If they aren't the same size, they aren't buddies. */
-        if (memory.getSize() != size) {
-            return null;
-        }
-
         /* See if this is on odd or even side of split */
         /* to determine the buddy's address.           */
-        int buddyAddress = (isEvenBuddy() ? address + size : address - size);
+        int buddyAddress = (isEvenBuddy() ? address + getSize() : address - getSize());
 
         if (buddyAddress != memory.getAddress()) {
             return null;
@@ -51,7 +63,8 @@ public class Memory {
 
         /* Set the new address appropriately and double the size. */
         address = (isEvenBuddy() ? address : memory.getAddress());
-        size = size * 2;
+        /* Size is stored as power of two, increment to double size. */
+        size++;
 
         /* This memory block has been adjusted to be   */
         /* the new merged memory block. Return itself. */
@@ -59,7 +72,7 @@ public class Memory {
     }
 
     private boolean isEvenBuddy() {
-        return (address / size % 2 == 0);
+        return (((address >> size) & 1) == 0);
     }
 
     public int getAddress() {
@@ -67,6 +80,6 @@ public class Memory {
     }
 
     public int getSize() {
-        return size;
+        return 1 << size;
     }
 }
